30f02c52f35955d6f0a439ca9f0fe9b2a45ee1fa
Tim Schoen <schoentr@gmail.com>
2019-02-06 02:43:15 -0800

    roster is being displayed

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
- 'use strict';
+'use strict';
 
 // Application Dependencies
 const express = require('express');
@@ -20,10 +20,16 @@ app.use(express.static('./public'));
 app.get('/', homePage);
 app.get('/game',getGame);
 app.get('/game/:id',getGameDetails);
+app.get('/teams/:id',getTeam);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 let gameDetail = [];
+let rosterTemp ={};
+// let rosterTemp.players =[];
+let rosterTempplayers= [];
+let teamId = '';
+var index = 0;
 
 function handleError(error, response){
   console.log(error);
@@ -34,19 +40,18 @@ function homePage(req,res){
   res.render('./index');
 }
 
-var index = 0;
 function getGameDetails(req,res){
-  console.log(req.params.id);
-  console.log(req.params.id.length);
- index = req.params.id;
+  // console.log(req.params.id);
+  // console.log(req.params.id.length);
+  index = req.params.id;
   // const index = (req.parms.id);
- console.log('index', index);
-  
-  console.log('line 39', gameDetail[index]);
+  //  console.log('index', index);
+
+  // console.log('line 39', gameDetail[index]);
   const singleGameObject = gameDetail[index];
   // console.log('ln42', singleGameObject);
-    return res.render('./detail',{singleGameObject:singleGameObject});
-// return res.redirect('./../game')
+  return res.render('./detail',{singleGameObject:singleGameObject});
+  // return res.redirect('./../game')
 
 }
 
@@ -54,9 +59,42 @@ function getGame(req,res){
   Game.lookupGame(req.query.data)
     .then((results) => {
       return res.render('./game11',{results:results});
-    })    
+    })
 }
 
+function getTeam (req,res){
+  // console.log(req.params.id);
+  teamId='';
+  teamId=req.params.id;
+  Player.lookupPlayer(req.query.data)
+    .then ((results) => {
+      return res.render ('./team',{rosterTemp:rosterTemp});
+    });
+}
+
+
+
+Player.lookupPlayer = (query) => {
+  // console.log('getTeam');
+  // console.log(gameDetail);
+
+  const url = `https://api.sportradar.us/mlb/trial/v6.5/en/teams/${teamId}/profile.json?api_key=${process.env.SPORTSRADAR_API}`;
+  return superagent.get(url)
+    .then((apiResponse) => {
+      rosterTemp= {};
+      rosterTemp.name = apiResponse.body.name;
+      console.log(rosterTemp.name);
+      // console.log(apiResponse.body.players[0]);
+      let playerArr = apiResponse.body.players.map((player) =>{
+        const singlePlayer = new Player(player);
+        return singlePlayer;
+      });
+      // console.log(playerArr);
+      rosterTemp.players = playerArr;
+      console.log(rosterTemp);
+      return playerArr;
+    });
+}
 Game.lookupGame = (query) => {
   let month = 6;
   let year = 2014;
@@ -71,17 +109,31 @@ Game.lookupGame = (query) => {
         let dailyGames =
       apiResponse.body.league.games.map((singleGame) => {
         const singleGameDetails = new Game( singleGame);
-        
+
         return singleGameDetails;
       });
-       
+
         // console.log('ln 69',gameDetail);
-                return dailyGames;
+        return dailyGames;
       }
     })
 
-  
+
 }
+function Player(data){
+  // console.log ('data',data);
+  this.id = data.id;
+  this.position = data.primary_position;
+  this.height =data.height;
+  this.weight = data.weight;
+  this.name = data.full_name;
+  this.bats = data.bat_hand;
+  this.throw = data.throw_hand;
+  this.birthday = data.birthdate;
+  rosterTempplayers.push(this);
+}
+
+
 
 function Game ( data){
   this.gameId = data.game.id;
@@ -103,5 +155,3 @@ function Game ( data){
 
   gameDetail.push(this);// console.log(this.?);
 }
-
-
